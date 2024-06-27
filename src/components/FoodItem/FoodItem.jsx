e187176d5658ecd1e2344e81606e50943781636f
Vaibhav Pandey <89473295+Vaibhavpan02@users.noreply.github.com>
2024-06-27 12:03:59 +0530

    Update FoodItem.jsx

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,36 +1,48 @@
-import React, { useContext } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext, useState } from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
 const FoodItem = ({id,name,price,description,image}) => {
-    
-    const {cartItems,addToCart,removeFromCart}=useContext(StoreContext);
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+    const [popup, setPopup] = useState({ show: false, message: '' });
 
-  return (
-    <div className='food-item'>
-        <div className="food-item-img-container">
-            <img className='food-item-image' src={image} alt="" />
-            {!cartItems[id]
-                ? <img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=''/>: <div className="food-item-counter">
-                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-                    <p>{cartItems[id]}</p>
-                    <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
-                </div>
-            }
-        </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
+    const handleAddToCart = (id) => {
+        addToCart(id);
+        setPopup({ show: true, message: `${name} added to cart` });
+        setTimeout(() => setPopup({ show: false, message: '' }), 2000); // Hide popup after 2 seconds
+    };
+
+    const handleRemoveFromCart = (id) => {
+        removeFromCart(id);
+        setPopup({ show: true, message: `${name} removed from cart` });
+        setTimeout(() => setPopup({ show: false, message: '' }), 2000); // Hide popup after 2 seconds
+    };
+
+    return (
+        <div className='food-item'>
+            <div className="food-item-img-container">
+                <img className='food-item-image' src={image} alt={name} />
+                {!cartItems[id]
+                    ? <img className='add' onClick={() => handleAddToCart(id)} src={assets.add_icon_white} alt='Add' />
+                    : <div className="food-item-counter">
+                        <img onClick={() => handleRemoveFromCart(id)} src={assets.remove_icon_red} alt="Remove" />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={() => handleAddToCart(id)} src={assets.add_icon_green} alt="Add" />
+                    </div>
+                }
             </div>
-            <p className="food-item-desc">
-                {description}
-            </p>
-            <p className="food-item-price">${price}</p>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p>
+                    <img src={assets.rating_starts} alt="Rating" />
+                </div>
+                <p className="food-item-desc">{description}</p>
+                <p className="food-item-price">${price}</p>
+            </div>
+            {popup.show && <div className="popup">{popup.message}</div>}
         </div>
-    </div>
-  )
+    );
 }
 
-export default FoodItem
+export default FoodItem;
